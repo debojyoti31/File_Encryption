623493c85d7e70e47c1bfd4a79e3a1395fc18800
Debojyoti Mondal <97456082+debojyoti31@users.noreply.github.com>
2023-12-14 17:55:31 +0530

    Update streamlitkey.py

--- streamlitkey.py
+++ streamlitkey.py
@@ -11,12 +11,12 @@ st.title('Simple File Encryption WebApp')
 how_to_use_expander = st.expander("How to Use", expanded=False)
 with how_to_use_expander:
     st.write("This simple File Encryption WebApp allows you to perform three operations:")
-    st.write("1. **Encryption**: Upload a file and a key, and click 'Encrypt' to generate an encrypted file.")
-    st.write("2. **Decryption**: Upload an encrypted file and its corresponding key, and click 'Decrypt' to get the original file.")
+    st.write("1. **Encryption**: Upload a file or write text, and a key. Click 'Encrypt' to generate an encrypted file.")
+    st.write("2. **Decryption**: Upload an encrypted file or write text, and its corresponding key. Click 'Decrypt' to get the original file.")
     st.write("3. **Generate New Key**: Enter a key name and click 'Generate New Key' to create a new encryption key.")
 
     st.write("Make sure to follow these steps:")
-    st.write("1. Upload the correct files for the chosen operation.")
+    st.write("1. Upload the correct files for the chosen operation or write text.")
     st.write("2. For Encryption and Decryption, ensure that the key matches the operation.")
     st.write("3. Download the result after the operation is complete.")
 
@@ -29,48 +29,70 @@ with st.container():
     # Encryption section
     if option == 'Encryption':
         enc_file = None
-        uploaded_file = st.file_uploader("Upload file Here")
+        upload_option = st.radio("Select input type:", ('Upload File', 'Write Text'))
+
+        if upload_option == 'Upload File':
+            uploaded_file = st.file_uploader("Upload file Here")
+            file_content = uploaded_file.read() if uploaded_file is not None else None
+
+        elif upload_option == 'Write Text':
+            file_content = st.text_area("Enter Text Here")
+
         uploaded_key = st.file_uploader("Upload Key Here")
 
-        if uploaded_key is not None and uploaded_file is not None:
+        if uploaded_key is not None and file_content is not None:
             if st.button('Encrypt'):
                 with st.spinner('Encrypting....'):
                     try:
                         cipher_suite = Fernet(uploaded_key.read())
-                        file_content = uploaded_file.read()
                         enc_file = cipher_suite.encrypt(file_content)
                     except Exception as e:
                         st.write(f'Error: {e}')
 
         if enc_file is not None:
-            st.download_button(
-                label="Download Encrypted File",
-                data=enc_file,
-                file_name=uploaded_file.name
-            )
+            if upload_option == 'Upload File':
+                st.download_button(
+                    label="Download Encrypted File",
+                    data=enc_file,
+                    file_name=uploaded_file.name
+                )
+            elif upload_option == 'Write Text':
+                st.write("Encrypted Text:")
+                st.text(enc_file)
 
     # Decryption section
     if option == 'Decryption':
         dec_file = None
-        uploaded_file = st.file_uploader("Upload file Here")
+        upload_option = st.radio("Select input type:", ('Upload File', 'Write Text'))
+
+        if upload_option == 'Upload File':
+            uploaded_file = st.file_uploader("Upload file Here")
+            file_content = uploaded_file.read() if uploaded_file is not None else None
+
+        elif upload_option == 'Write Text':
+            file_content = st.text_area("Enter Text Here")
+
         uploaded_key = st.file_uploader("Upload Key Here")
 
-        if uploaded_key is not None and uploaded_file is not None:
+        if uploaded_key is not None and file_content is not None:
             if st.button('Decrypt'):
                 with st.spinner('Decrypting....'):
                     try:
                         cipher_suite = Fernet(uploaded_key.read())
-                        file_content = uploaded_file.read()
                         dec_file = cipher_suite.decrypt(file_content)
                     except Exception as e:
                         st.write(f'Error: {e}')
 
         if dec_file is not None:
-            st.download_button(
-                label="Download Decrypted File",
-                data=dec_file,
-                file_name=uploaded_file.name
-            )
+            if upload_option == 'Upload File':
+                st.download_button(
+                    label="Download Decrypted File",
+                    data=dec_file,
+                    file_name=uploaded_file.name
+                )
+            elif upload_option == 'Write Text':
+                st.write("Decrypted Text:")
+                st.text(dec_file)
 
     # Generate New Key section
     if option == 'Generate New Key':
